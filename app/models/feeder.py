40c064d6399d55642472205a3d4ebc5927c244f6
SirCaptainMitch <hamann.mitchell@gmail.com>
2020-05-09 11:53:37 -0500

    adding logic to fire servo

--- app/models/feeder.py
+++ app/models/feeder.py
@@ -2,7 +2,7 @@ from app.db import db
 import datetime
 import time
 import sys
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from sqlalchemy import Column, Integer, DateTime, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -32,11 +32,27 @@ class FeederModel(db.Model):
 
   @classmethod
   def feed(cls, attempts):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
     try: 
-      ## This is where the catomatic logic will go.      
-      return True
+      ## This is where the catomatic logic will go.
+      servo = GPIO.PWM(12, 50)
+      servo.start(12.5)
+
+      # spin left, right, then left again rather than in a continuous circle
+      # to prevent the food from jamming the servo
+      for index in range(0, attempts):
+          dutyCycle = 2.5 if (index % 2 == 0) else 12.5
+          servo.ChangeDutyCycle(dutyCycle)
+          # adjust the sleep time to have the servo spin longer or shorter in that direction
+          time.sleep(.75)     
     except:
       return False
+    finally:
+        # always cleanup after ourselves
+        servo.stop()
+        GPIO.cleanup()
+        return True
 
   @classmethod
   def find_by_name(cls, name):
